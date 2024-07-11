7a8b6e9c122f3b23c41fe6c09920255859fb31bb
noctuelles <plouvel@student.42.fr>
2024-07-11 19:24:01 +0200

    feat: refactor bool evaluation

--- src/bool_eval/mod.rs
+++ src/bool_eval/mod.rs
@@ -6,7 +6,7 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 17:17:20 by plouvel           #+#    #+#             */
-/*   Updated: 2024/07/11 18:14:45 by plouvel          ###   ########.fr       */
+/*   Updated: 2024/07/11 19:23:14 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@ mod parser;
 
 use crate::binary_tree::BTreeNode;
 
-pub enum Operand {
+pub enum Operand<'a> {
     Immediate(bool),
-    Variable(bool),
+    Variable(&'a bool),
 }
 
 pub enum Operator {
@@ -29,9 +29,42 @@ pub enum Operator {
     Equi,
 }
 
-pub enum BoolEvalNodeValue {
-    Operand(Operand),
+pub enum BoolEvalNodeValue<'a> {
+    Operand(Operand<'a>),
     Operator(Operator),
 }
 
-pub type ASTBoolEval = BTreeNode<BoolEvalNodeValue>;
+pub type ASTBoolEval<'a> = BTreeNode<BoolEvalNodeValue<'a>>;
+
+impl<'a> BTreeNode<BoolEvalNodeValue<'a>> {
+    pub fn new(value: BoolEvalNodeValue<'a>) -> Self {
+        BTreeNode::<BoolEvalNodeValue<'a>> {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn eval(&self) -> bool {
+        match &self.value {
+            BoolEvalNodeValue::Operand(Operand::Immediate(b)) => *b,
+            BoolEvalNodeValue::Operand(Operand::Variable(b)) => **b,
+            BoolEvalNodeValue::Operator(Operator::And) => {
+                self.left.as_ref().unwrap().eval() && self.right.as_ref().unwrap().eval()
+            }
+            BoolEvalNodeValue::Operator(Operator::Or) => {
+                self.left.as_ref().unwrap().eval() || self.right.as_ref().unwrap().eval()
+            }
+            BoolEvalNodeValue::Operator(Operator::Xor) => {
+                self.left.as_ref().unwrap().eval() ^ self.right.as_ref().unwrap().eval()
+            }
+            BoolEvalNodeValue::Operator(Operator::Not) => !self.right.as_ref().unwrap().eval(),
+            BoolEvalNodeValue::Operator(Operator::Impl) => {
+                !self.left.as_ref().unwrap().eval() || self.right.as_ref().unwrap().eval()
+            }
+            BoolEvalNodeValue::Operator(Operator::Equi) => {
+                self.left.as_ref().unwrap().eval() == self.right.as_ref().unwrap().eval()
+            }
+        }
+    }
+}
--- src/bool_eval/parser.rs
+++ src/bool_eval/parser.rs
@@ -6,22 +6,37 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 17:59:19 by plouvel           #+#    #+#             */
-/*   Updated: 2024/07/11 18:16:50 by plouvel          ###   ########.fr       */
+/*   Updated: 2024/07/11 19:21:29 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+use std::collections::{HashMap, HashSet};
 
 use super::lexer::*;
 use super::*;
 use crate::binary_tree::BTreeNode;
 
-pub fn parse_ast(tkns: Vec<Token>) -> Result<ASTBoolEval, String> {
+pub fn parse_ast(tkns: Vec<Token>) -> Result<(ASTBoolEval, HashMap<char, bool>), String> {
     let mut output: Vec<ASTBoolEval> = Vec::new();
+    let letters: HashMap<char, bool> = HashMap::new();
 
     for tkn in tkns {
         match tkn {
             /* Operands */
-            Token::True | Token::False => output.push(BTreeNode {
+            Token::True => output.push(BTreeNode {
                 value: BoolEvalNodeValue::Operand(Operand::Immediate(true)),
+                right: None,
+                left: None,
+            }),
+            Token::False => output.push(BTreeNode {
+                value: BoolEvalNodeValue::Operand(Operand::Immediate(false)),
+                right: None,
+                left: None,
+            }),
+            Token::Letter(c) => output.push(BTreeNode {
+                value: BoolEvalNodeValue::Operand(Operand::Variable(
+                    letters.entry(c).or_insert(false),
+                )),
                 right: None,
                 left: None,
             }),
@@ -37,10 +52,54 @@ pub fn parse_ast(tkns: Vec<Token>) -> Result<ASTBoolEval, String> {
                     return Err(format!("{} requires one operand", tkn.to_string()));
                 }
             }
-            Token::And | Token::Or | Token::Xor | Token::Equi | Token::Impl => {
+            Token::And => {
                 if let (Some(a), Some(b)) = (output.pop(), output.pop()) {
                     output.push(BTreeNode {
-                        value: tkn,
+                        value: BoolEvalNodeValue::Operator(Operator::And),
+                        right: Some(Box::new(a)),
+                        left: Some(Box::new(b)),
+                    })
+                } else {
+                    return Err(format!("{} requires two operands", tkn.to_string()));
+                }
+            }
+            Token::Or => {
+                if let (Some(a), Some(b)) = (output.pop(), output.pop()) {
+                    output.push(BTreeNode {
+                        value: BoolEvalNodeValue::Operator(Operator::Or),
+                        right: Some(Box::new(a)),
+                        left: Some(Box::new(b)),
+                    })
+                } else {
+                    return Err(format!("{} requires two operands", tkn.to_string()));
+                }
+            }
+            Token::Xor => {
+                if let (Some(a), Some(b)) = (output.pop(), output.pop()) {
+                    output.push(BTreeNode {
+                        value: BoolEvalNodeValue::Operator(Operator::Xor),
+                        right: Some(Box::new(a)),
+                        left: Some(Box::new(b)),
+                    })
+                } else {
+                    return Err(format!("{} requires two operands", tkn.to_string()));
+                }
+            }
+            Token::Impl => {
+                if let (Some(a), Some(b)) = (output.pop(), output.pop()) {
+                    output.push(BTreeNode {
+                        value: BoolEvalNodeValue::Operator(Operator::Impl),
+                        right: Some(Box::new(a)),
+                        left: Some(Box::new(b)),
+                    })
+                } else {
+                    return Err(format!("{} requires two operands", tkn.to_string()));
+                }
+            }
+            Token::Equi => {
+                if let (Some(a), Some(b)) = (output.pop(), output.pop()) {
+                    output.push(BTreeNode {
+                        value: BoolEvalNodeValue::Operator(Operator::Equi),
                         right: Some(Box::new(a)),
                         left: Some(Box::new(b)),
                     })
@@ -53,5 +112,31 @@ pub fn parse_ast(tkns: Vec<Token>) -> Result<ASTBoolEval, String> {
     if output.len() != 1 {
         return Err(String::from("trailing operands"));
     }
-    Ok(output.pop().unwrap())
+    Ok((output.pop().unwrap(), letters))
+}
+
+mod tests {
+    mod parse_ast {
+        use super::super::*;
+
+        #[test]
+        fn valid() {
+            assert_eq!(
+                parse_ast(vec![Token::Letter('A'), Token::Letter('B'), Token::And]),
+                Ok(BTreeNode {
+                    value: BoolEvalNodeValue::Operator(Operator::And),
+                    right: Some(Box::new(BTreeNode {
+                        value: BoolEvalNodeValue::Operand(Operand::Immediate(false)),
+                        right: None,
+                        left: None,
+                    })),
+                    left: Some(Box::new(BTreeNode {
+                        value: BoolEvalNodeValue::Operand(Operand::Immediate(true)),
+                        right: None,
+                        left: None,
+                    })),
+                })
+            );
+        }
+    }
 }
